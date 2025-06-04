70757e0308fce714a67783913357a431ed1556b6
roshan22418 <roshan22418@iiitd.ac.in>
2025-06-04 19:02:04 +0530

    solve using fast and slow pointer

--- LinkedList/Problems/CheckLoopPresentORnotWherePresent.cpp
+++ LinkedList/Problems/CheckLoopPresentORnotWherePresent.cpp
@@ -7,45 +7,69 @@
  };
  */
 
+#include <iostream>
+using namespace std;
 
- #include<iostream>
- using namespace std;
+#include <map>
 
- #include<map>
-
-
-
-
- class ListNode{
-    public:
+class ListNode
+{
+public:
     int val;
-    ListNode* next;
-    ListNode(int data){
+    ListNode *next;
+    ListNode(int data)
+    {
         this->val = data;
         this->next = NULL;
     }
- };
+};
 
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return false;
+        }
 
-class Solution {
-    public:
+        // do this thing or do below thing but prefer below thing then slow and fast condition check below not above while loop
+        //  ListNode* slow = head;
+        //  ListNode* fast = head->next;
+        //  if(fast!=NULL){
+        //      slow = slow->next;
+        //      fast = fast->next;
+        //  }
+        ListNode *slow = head;
+        ListNode *fast = head;
 
-
-    bool hasCycle(ListNode *head) {
-        
-        map<ListNode*,int> mp;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            if(mp[temp]==0){
-                mp[temp] = 1;
+        while (fast != NULL)
+        {
+            fast = fast->next;
+            if (fast != NULL)
+            {
+                fast = fast->next;
+                slow = slow->next;
             }
-            else{
+            if (slow == fast)
+            {
                 return true;
             }
-            temp = temp->next;
         }
         return false;
 
-        
+        // map<ListNode*,int> mp;
+        // ListNode* temp = head;
+        // while(temp!=NULL){
+        //     if(mp[temp]==0){
+        //         mp[temp] = 1;
+        //     }
+        //     else{
+        //         return true;
+        //     }
+        //     temp = temp->next;
+        // }
+        // return false;
     }
 };
