40bb5964318d5ad27841bed90ef849fbf44ddced
roshan22418 <roshan22418@iiitd.ac.in>
2025-06-07 18:51:09 +0530

    create heap given array in O(n) time

---
+++ Heap/TheoryBasicImplementation/CreateHeapUsingArray.cpp
@@ -0,0 +1,46 @@
+
+#include<iostream>
+using namespace std;
+
+
+
+
+
+void Heapify(int arr[],int n,int index){
+    int leftIndex = 2*index;
+    int rightIndex = 2*index +1;
+    int largestIndex= index;
+    if(leftIndex<=n && arr[largestIndex]<arr[leftIndex]){
+        largestIndex = leftIndex;
+    }
+    if(rightIndex<=n && arr[largestIndex]<arr[rightIndex]){
+        largestIndex = rightIndex;
+    }
+    if(index != largestIndex){
+        swap(arr[index],arr[largestIndex]);
+        index = largestIndex;
+        Heapify(arr,n,index);
+    }
+    else{
+        return;
+    }
+}
+
+
+void buildHeap(int arr[],int n){
+    for(int i = (n/2);i>0;i--){
+        Heapify(arr,n,i);
+    }
+    
+}
+
+
+int main(){
+    int arr[] = {-1,5,10,15,20,25,12};
+    int size = 6;
+    buildHeap(arr,size);
+    for(int i = 1;i<=size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
