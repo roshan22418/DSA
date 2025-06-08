fd07794e82f1d7e39dac8cfbc1cc6ddb7d30aaf8
roshan22418 <roshan22418@iiitd.ac.in>
2025-06-08 11:49:37 +0530

    modify the code

--- Stack/Problems/validParenthesis.cpp
+++ Stack/Problems/validParenthesis.cpp
@@ -2,6 +2,53 @@
 #include <stack>
 using namespace std;
 
+
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int index = 0;
+        int size1 = s.size();
+        while(index<size1){
+            if(st.empty()){
+                if(s[index] == '(' || s[index] == '{' || s[index] == '['){
+                    st.push(s[index]);
+                    index ++;
+                }
+                else{
+                    return false;     
+                }
+                           
+            }
+            else if(s[index] == '(' || s[index] == '{' || s[index] == '['){
+                st.push(s[index]);
+                index ++;
+            }
+            else{
+                if((st.top() =='(' && s[index] ==')') || (st.top() =='{' && s[index] =='}') || (st.top() =='[' && s[index] ==']')){
+                    st.pop();
+                    index++;
+                }
+                else{
+                    return false;
+                }
+
+            }
+            
+        }
+        if(st.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+        
+    }
+};
+
 bool validParenthesis(string str)
 {
     stack<char> st;
