37d2c68c8af823d93e51ac1b70ebf6760ae6706b
roshan22418 <roshan22418@iiitd.ac.in>
2025-06-11 17:24:19 +0530

    add GCD code

---
+++ Arrays/BasicMathAndPointer/GCD_LCM.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+//by using recurssion
+
+int gcdByRecurssion(int a,int b){
+    if(a == 0 || b == 0){
+        return a == 0 ? b : a;
+    }
+    if(a>b){
+        return gcdByRecurssion(a-b,b);
+    }
+    else{
+        return gcdByRecurssion(b-a,a);
+    }
+}
+
+
+
+//by using loop
+
+
+int gcd(int a,int b){
+    if(a ==0 || b == 0){
+        return a == 0 ? b :a;
+    }
+    while (a != 0 && b != 0)
+    {
+        if(a>b){
+            a = a -b;
+        }
+        else{
+            b = b -a;
+        }
+    }
+    return a == 0 ? b : a;
+    
+}
+
+
+
+int main(){
+    int a = 0;
+    int b = 12;
+    int gcdAns = gcdByRecurssion(a,b);
+    cout<<gcdAns<<endl;
+    return 0;
+}
